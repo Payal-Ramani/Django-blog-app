8caeda5be031ed66a31f9aa56773bd942fbad564
Payal Ramani <payalr.tagline@gmail.com>
2023-01-16 16:02:41 +0530

    made authordetailpage

--- myproject/blog/urls.py
+++ myproject/blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('',views.HomePage.as_view(),name='homepage'),
     path('blog/',views.HomePage.as_view(),name='homepage'),
     path('blog/blogs/',views.BlogListView.as_view(),name='bloglistview'),
-    path('blog/<pk>/',views.BlogDetailView.as_view(),name='blogdetailview'),
-    path('blog/blogger/<pk>',views.AuthorDetailView.as_view(),name='authordetailview')
+    path('blog/<int:pk>/',views.BlogDetailView.as_view(),name='blogdetailview'),
+    path('blog/blogger/<int:pk>/',views.AuthorDetailView.as_view(),name='authordetailview')
 ]
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,17 +1,13 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
-from django.shortcuts import render
 from django.views.generic import TemplateView,ListView,detail
 
-from .models import Blogs,Authors
+from .models import Blogs
+from .models import Authors
 
 class HomePage(TemplateView):
     template_name = 'blog/homepage.html'
 
 class BlogListView(ListView):
     template_name = 'blog/bloglistpage.html'
-    # context_object_name = 'blog_list'
-
     queryset = Blogs.objects.order_by('-posted_date')
     paginate_by = 5
 
@@ -19,16 +15,17 @@ class BlogDetailView(detail.DetailView):
     model = Blogs
     template_name = 'blog/blogdetailpage.html'
 
-class AuthorDetailView(detail.DetailView):
-    model = Authors
+class AuthorDetailView(ListView):
+    model = Blogs
     template_name = 'blog/authordetailpage.html'
+    context_object_name = 'author_detail'
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(author_id=self.kwargs["pk"]).order_by('-posted_date')
+        return queryset
 
-
-
-# def listing(request):
-#     contact_list = Blogs.objects.all()
-#     paginator = Paginator(contact_list, 4)
-
-#     page_number = request.GET.get('p')
-#     page_obj = paginator.get_page(page_number)
-#     return render(request, 'blog/bloglistpage.html', {'page_obj': page_obj})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["context"] = Authors.objects.filter(id=self.kwargs["pk"]).first()
+        return context
