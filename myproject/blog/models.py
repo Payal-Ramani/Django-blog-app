8347c986f8a2cef4377dd8b7d3fa2c41f6386e3d
Payal Ramani <payalr.tagline@gmail.com>
2023-01-17 15:20:18 +0530

    changed the model names from Authors to Author and Blogs to Blog as well as changing the place of import module , removed url from blogdetailpage

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from datetime import datetime
 
-class Authors(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=100)
     about_author = models.TextField()
 
     def __str__(self):
         return self.name
 
-class Blogs(models.Model):
-    author = models.ForeignKey(Authors,on_delete=models.CASCADE)
+class Blog(models.Model):
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     posted_date = models.DateTimeField(default=datetime.now())
     content = models.TextField()
@@ -18,7 +18,7 @@ class Blogs(models.Model):
         return self.title
 
 class Comment(models.Model):
-    blog = models.ForeignKey(Blogs,on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
     comment_content = models.TextField()
     commented_date = models.DateTimeField(default=datetime.now())
 
