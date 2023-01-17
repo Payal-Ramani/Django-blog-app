a5e3085eb1c0408446787c1dfd5716060f0a2b87
Payal Ramani <payalr.tagline@gmail.com>
2023-01-17 11:35:13 +0530

    blogs detail page is updated with comments and author name

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Blogs,Authors
+from .models import Blogs,Authors,Comment
 
 admin.site.register(Blogs)
 admin.site.register(Authors)
+admin.site.register(Comment)
--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -13,3 +13,14 @@ class Blogs(models.Model):
     title = models.CharField(max_length=100)
     posted_date = models.DateTimeField(default=datetime.now())
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blogs,on_delete=models.CASCADE)
+    comment_content = models.TextField()
+    commented_date = models.DateTimeField(default=datetime.now())
+
+    def __str__(self):
+        return self.comment_content
